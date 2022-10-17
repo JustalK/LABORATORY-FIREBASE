58ccbd7f52c2ce16e852752ce3007494be72b7c8
justalk <justal.kevin@gmail.com>
2022-10-17 16:41:28 +0200

    [FEATURE] Creating a working experimental function

--- functions/index.js
+++ functions/index.js
@@ -1,97 +1,33 @@
-//import libraries
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as express from "express";
-import * as bodyParser from "body-parser";
+const functions = require("firebase-functions");
 
-//initialize firebase inorder to access its services
-admin.initializeApp(functions.config().firebase);
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+const config = {
+  apiKey: "AIzaSyDOsAb82j7FYmCsa_sJB3p3CPquqyppikg",
+  authDomain: "stoked-brand-140404.firebaseapp.com",
+  projectId: "stoked-brand-140404",
+  storageBucket: "stoked-brand-140404.appspot.com",
+  messagingSenderId: "548932435382",
+  appId: "1:548932435382:web:b198355119f26f70b79517",
+};
+admin.initializeApp(config);
 
-//initialize express server
-const app = express();
-const main = express();
+exports.test = functions.https.onRequest(async (req, res) => {
+  console.log("test");
 
-//add the path to receive request and set json as bodyParser to process the body
-main.use("/api/v1", app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
-
-//initialize the database and the collection
-const db = admin.firestore();
-const userCollection = "users";
-
-// Create new user
-app.post("/users", async (req, res) => {
-  try {
-    const user = {
-      firstName: req.body["firstName"],
-      lastName: req.body["lastName"],
-      email: req.body["email"],
-      areaNumber: req.body["areaNumber"],
-      department: req.body["department"],
-      id: req.body["id"],
-      contactNumber: req.body["contactNumber"],
-    };
-
-    const newDoc = await db.collection(userCollection).add(user);
-    res.status(201).send(`Created a new user: ${newDoc.id}`);
-  } catch (error) {
-    res
-      .status(400)
-      .send(
-        `User should cointain firstName, lastName, email, areaNumber, department, id and contactNumber!!!`
-      );
-  }
+  res.json({ result: true });
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const userQuerySnapshot = await db.collection(userCollection).get();
-    const users = [];
-    userQuerySnapshot.forEach((doc) => {
-      users.push({
-        id: doc.id,
-        data: doc.data(),
-      });
-    });
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await admin
+    .firestore()
+    .collection("messages")
+    .add({ original: original });
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
-
-app.get("/users/:userId", (req, res) => {
-  const userId = req.params.userId;
-  db.collection(userCollection)
-    .doc(userId)
-    .get()
-    .then((user) => {
-      if (!user.exists) throw new Error("User not found");
-      res.status(200).json({ id: user.id, data: user.data() });
-    })
-    .catch((error) => res.status(500).send(error));
-});
-
-// Delete a user
-app.delete("/users/:userId", (req, res) => {
-  db.collection(userCollection)
-    .doc(req.params.userId)
-    .delete()
-    .then(() => res.status(204).send("Document successfully deleted!"))
-    .catch(function (error) {
-      res.status(500).send(error);
-    });
-});
-
-// Update user
-app.put("/users/:userId", async (req, res) => {
-  await db
-    .collection(userCollection)
-    .doc(req.params.userId)
-    .set(req.body, { merge: true })
-    .then(() => res.json({ id: req.params.userId }))
-    .catch((error) => res.status(500).send(error));
-});
-
-//define google cloud function name
-export const webApi = functions.https.onRequest(main);
