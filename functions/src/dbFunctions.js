927eb220777cbde0e55ba577d99e6fbf21917eae
justalk <justal.kevin@gmail.com>
2022-10-19 15:51:02 +0200

    [FEATURE] Adding database

--- functions/src/dbFunctions.js
+++ functions/src/dbFunctions.js
@@ -19,11 +19,42 @@ const app = admin.initializeApp(config);
 const db = getFirestore(app);
 
 module.exports = function (e) {
+  /**
+   * Get the data from one particular collection
+   */
   e.getUsers = functions.https.onRequest(async (req, res) => {
     const snapshot = await db.collection("user").get();
     snapshot.forEach((doc) => {
       console.log(doc.id, "=>", doc.data());
     });
+    res.json({ result: true });
+  });
+
+  /**
+   * https://firebase.google.com/docs/firestore/manage-data/add-data
+   * Save something in the database
+   */
+  e.saveNewUser = functions.https.onRequest(async (req, res) => {
+    const docRef = db.collection("user").doc("alovelace");
+
+    await docRef.set({
+      first: "Ada2",
+      last: "Lovelace",
+      born: 1815,
+    });
+    res.json({ result: true });
+  });
+
+  /**
+   * Save something in the database
+   */
+  e.saveNewUser2 = functions.https.onRequest(async (req, res) => {
+    db.collection("user").add({
+      first: "Ada2",
+      last: "Lovelace",
+      born: 1815,
+    });
+
     res.json({ result: true });
   });
 };
